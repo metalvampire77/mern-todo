5185b6e5e098877ab4c5deaacd5346e4dfc0d5aa
metalvampire77 <68247894+metalvampire77@users.noreply.github.com>
2023-10-27 16:45:23 +0530

    update css file

--- server/index.js
+++ server/index.js
@@ -1,78 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const TodoModel = require("./models/todo");
+const path = require("path");
+const dotenv = require("dotenv");
 
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const TodoModel = require('./models/todo')
-const path = require('path')
-const dotenv = require('dotenv')
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+dotenv.config();
 
-dotenv.config()
+const URI = process.env.MONGO_URI;
 
-const URI = process.env.MONGO_URI
+mongoose
+  .connect(URI, {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`connected`);
+  })
+  .catch((err) => console.log(err));
 
-mongoose.connect(URI,{
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log(`connected`)
-})
-.catch(err => console.log(err))
+const db = mongoose.connection;
 
-const db = mongoose.connection
+db.once("open", () => console.log("db connected"));
 
-db.once('open',() => console.log('db connected'))
-
-app.get('/get',(req,res) => {
-    TodoModel.find()
+app.get("/get", (req, res) => {
+  TodoModel.find()
     .then((result) => {
-        res.json(result)
+      res.json(result);
     })
-    .catch(err => res.json(err))
-})
-
-// Serve the React application
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-
-// For any other route, serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    .catch((err) => res.json(err));
 });
 
-const PORT = process.env.PORT
+// Serve the React application
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.listen(PORT,() => {
-    console.log(`server running on port ${PORT} `)
-})
+// For any other route, serve the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
-app.put('/update/:id',(req,res) => {
-    const {id} = req.params
-    const {done} = req.body
-    TodoModel.findByIdAndUpdate({_id: id},{done: !done}, { new: true })
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT} `);
+});
+
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { done } = req.body;
+  TodoModel.findByIdAndUpdate({ _id: id }, { done: !done }, { new: true })
     .then((result) => {
-        res.json(result)
+      res.json(result);
     })
-    .catch(err => res.json(err))
-})
+    .catch((err) => res.json(err));
+});
 
-app.delete('/delete/:id',(req,res) => {
-    const {id} = req.params
-    TodoModel.findByIdAndDelete({_id: id})
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndDelete({ _id: id })
     .then((result) => {
-        res.json(result)
+      res.json(result);
     })
-    .catch(err => res.json(err))
-})
+    .catch((err) => res.json(err));
+});
 
-app.post('/',(req,res) => {
-    TodoModel.create(req.body)
-    .then(() => res.json(req.body))
-})
-
-
-
-
+app.post("/", (req, res) => {
+  TodoModel.create(req.body).then(() => res.json(req.body));
+});
